097b2d4998bb248d3d09940a4d1ec7071a09b61c
Engr. Kirk Alyn Santos <kirkalyn13@gmail.com>
2021-09-09 23:59:01 +0800

    add single view equipment

--- src/components/Equipment.js
+++ src/components/Equipment.js
@@ -1,14 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect,useContext } from 'react'
 import axios from 'axios'
 import Button from '@material-ui/core/Button'
 import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn'
+import { EquipmentContext } from './View'
 
 const Equipment = ({id}) => {
+    const { setShowEquipment, setEqpID } = useContext(EquipmentContext)
     const [ equipment, setEquipment ] = useState({})
     const getEquipData = () => {
     axios.get(`http://localhost:3005/equipment/${id}`).then((response)=>{
         setEquipment(response.data[0])
         })
+    }
+    const returnToView = () => {
+        setShowEquipment(false)
+        setEqpID()
     }
     useEffect(()=>{
         getEquipData()
@@ -24,52 +30,52 @@ const Equipment = ({id}) => {
         </div>
             <div className="container-details">
                 <div className="details">
-                    <div className="details-column">
+                    <div className="details-column-info">
                         <div className="detail-title">
                             <img className="section-logo" src="/img/info.png" alt="" height="50px" width="50px" />
                             <h2 color="#FFFFFF">General Information</h2>
                         </div>
-                        <label>Name: {equipment.name}</label>
-                        <label>Type: </label>
-                        <label>Model: </label>
-                        <label>Serial: </label>
-                        <label>Description: </label>
+                        <div className="view-info-label"><p>Name: </p><p>{equipment.name}</p></div>
+                        <div className="view-info-label"><p>Type: </p><p>{equipment.type}</p></div>
+                        <div className="view-info-label"><p>Model: </p><p>{equipment.model}</p></div>
+                        <div className="view-info-label"><p>Serial: </p><p>{equipment.serial}</p></div>
+                        <div className="view-info-label"><p>Description: </p><p>{equipment.description}</p></div>
                     </div>
-                    <div className="details-column">
+                    <div className="details-column-info">
                         <div className="detail-title">
                             <img className="section-logo" src="/img/purchase.png" alt="" height="50px" width="50px" />
                             <h2 color="#FFFFFF">Purchase Details</h2>
                         </div>
-                        <label>Brand: </label>
-                        <label>Manufacturer: </label>
-                        <label>Price: </label>
-                        <label>Expiration: </label>
-                        <label>Purchase Date: </label>
+                        <div className="view-info-label"><p>Brand: </p><p>{equipment.brand}</p></div>
+                        <div className="view-info-label"><p>Manufacturer: </p><p>{equipment.manufacturer}</p></div>
+                        <div className="view-info-label"><p>Price: </p><p>{equipment.price}</p></div>
+                        <div className="view-info-label"><p>Expiration: </p><p>{equipment.expiration}</p></div>
+                        <div className="view-info-label"><p>Purchase Date: </p><p>{equipment.purchaseDate}</p></div>
                     </div>
                 </div>
             </div>
             <div className="container-details">
                 <div className="details">
-                    <div className="details-column">
+                    <div className="details-column-info">
                         <div className="detail-title">
                             <img className="section-logo" src="/img/calibration.png" alt="" height="50px" width="50px" />
                             <h2 color="#FFFFFF">Calibration Details</h2>
                         </div>
-                        <label>Calibration Date: </label>
-                        <label>Next Calibration Date: </label>
-                        <label>Calibration Method: </label>
-                        <label>Certificate: </label>
+                        <div className="view-info-label"><p>Calibration Date: </p><p>{equipment.calibrationDate}</p></div>
+                        <div className="view-info-label"><p>Next Calibration Date: </p><p>{equipment.nextCalibration}</p></div>
+                        <div className="view-info-label"><p>Calibration Method: </p><p>{equipment.calibrationMethod}</p></div>
+                        <div className="view-info-label"><p>Certificate: </p></div>
                     </div>
-                    <div className="details-column">
+                    <div className="details-column-info">
                         <div className="detail-title">
                             <img className="section-logo" src="/img/others.png" alt="" height="50px" width="50px" />
                             <h2 color="#FFFFFF">Other Details</h2>
                         </div>
-                        <label>Issued By: </label>
-                        <label>Issued To: </label>
-                        <label>Location: </label>
-                        <label>Status: </label>  
-                        <label>Remarks: </label>
+                        <div className="view-info-label"><p>Issued By: </p><p>{equipment.issuedBy}</p></div>
+                        <div className="view-info-label"><p>Issued To: </p><p>{equipment.issuedTo}</p></div>
+                        <div className="view-info-label"><p>Location: </p><p>{equipment.location}</p></div>
+                        <div className="view-info-label"><p>Status: </p><p>{equipment.status}</p></div>
+                        <div className="view-info-label"><p>Remarks: </p><p>{equipment.remarks}</p></div>
                     </div>
                 </div>
             </div>
@@ -78,8 +84,9 @@ const Equipment = ({id}) => {
                 variant="contained"
                 style={{backgroundColor: '#FFA000', color: '#000', fontWeight:"bold"}}
                 startIcon={<KeyboardReturnIcon />}
+                onClick={returnToView}
                 >
-                UPDATE EQUIPMENT INFO
+                RETURN
                 </Button>
             </div>
     </div>
