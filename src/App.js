f14f1b3091d204bb6f39cf048a9c27037df12335
Kirk Santos <kirkalyn13@gmail.com>
2023-03-06 21:15:00 +0800

    chore: changed server ao 030623

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import ProtectedRoute from './routes/ProtectedRoute'
 const SCHOOL = "School of Engineering and Architecture"
 export const DEPT = "SEA Lab"
 // export const SERVER = "localhost:3005"  //Edit for deployment
-export const SERVER = "b76a-120-29-79-81.ap.ngrok.io" // For deployment
+export const SERVER = "0722-120-29-79-81.ngrok.io" // For deployment
 export const LoginContext = React.createContext()
 
 
