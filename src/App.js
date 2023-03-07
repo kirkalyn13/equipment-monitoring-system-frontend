7bba1c76f34451277e960a0715681071bd34d045
Kirk Santos <kirkalyn13@gmail.com>
2023-03-07 09:41:17 +0800

    feat: added sidebar active styles

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import ProtectedRoute from './routes/ProtectedRoute'
 const SCHOOL = "School of Engineering and Architecture"
 export const DEPT = "SEA Laboratory"
 // export const SERVER = "localhost:3005"  //Edit for deployment
-export const SERVER = "0722-120-29-79-81.ngrok.io" // For deployment
+export const SERVER = "c505-120-29-79-81.ngrok.io" // For deployment
 export const LoginContext = React.createContext()
 
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 const Sidebar = ({role}) => {
     const editors = [ "admin", "super" ]
+    const [active, setActive ] = useState("all")
+    const location = useLocation()
+
+    // Handle active from current location
+    useEffect(() => {
+        setActive(location.pathname)
+    },[location])
+
     return (
         <div className="container-nav">
             <nav>
-                <Link to="/">
+                <Link to="/"
+                className={active === "/" ? 'nav-active' : ``}>
                     DASHBOARD
                 </Link>
-                <Link to="/view">
+                <Link to="/view"
+                className={active === "/view" ? 'nav-active' : ``}>
                     VIEW EQUIPMENT
                 </Link>
-                {editors.includes(role) ? (<Link to="/add">
+                {editors.includes(role) ? (<Link to="/add"
+                className={active === "/add" ? 'nav-active' : ``}>
                     ADD EQUIPMENT
                 </Link>) : null}
-                {editors.includes(role) ? (<Link to="/manage">
+                {editors.includes(role) ? (<Link to="/manage"
+                className={active === "/manage" ? 'nav-active' : ``}>
                     MANAGE EQUIPMENT
                 </Link>) : null}
-                {role === "super" ? (<Link to="/users">
+                {role === "super" ? (<Link to="/users"
+                className={active === "/users" ? 'nav-active' : ``}>
                     MANAGE USERS
                 </Link>) : null}
             </nav>
