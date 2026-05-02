c06f2f16da4e68e450f06a946891d0e1dbcc38fc
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-05-02 03:28:53 -0400

    fix: minor url config changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,3 @@
-export const URL = process.env.REACT_APP_NODE_ENV !== "development" ? process.env.REACT_APP_BACKEND_SERVER_URL : "http://localhost:3005/api/v1"
+export const URL = process.env.REACT_APP_NODE_ENV === "development" 
+  ? "http://localhost:3005/api/v1" 
+  : process.env.REACT_APP_BACKEND_SERVER_URL
