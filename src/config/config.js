569900cfc42f8bb6e27da049773d61a7538995e1
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-05-02 03:01:59 -0400

    feat: update env var names

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,1 @@
-export const URL = process.env.NODE_ENV !== "development" ? process.env.BACKEND_SERVER_URL : "http://localhost:3005/api/v1"
+export const URL = process.env.REACT_APP_NODE_ENV !== "development" ? process.env.REACT_APP_BACKEND_SERVER_URL : "http://localhost:3005/api/v1"
