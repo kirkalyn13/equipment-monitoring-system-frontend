33df7b4a4571cf29c36c4b75575b8bf4534c9a0a
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-05-03 20:00:02 -0400

    feat: loading color

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import CircularProgress from '@mui/material/CircularProgress'
 const Loading = ({offset = false}) => {
   return (
     <div className='container-loading'>
-      <CircularProgress color="inherit" size="3rem"  sx={offset ? { mt: '-20%' } : null}/>
+      <CircularProgress size="3rem" sx={{ color: '#FFAA00', ...(offset ? { mt: '-20%' } : {}) }} />
     </div>
   )
 }
